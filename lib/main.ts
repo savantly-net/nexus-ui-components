5c8f5de0fab5822e1050f704129df0fbb4f66b5c
Jeremy <jeremy@savantly.net>
2024-03-11 02:14:16 -0500

    refactor for easier integration

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,3 @@
-// Main CSS file
-export * from "./main.css";
-
-// Themes
-export * from "./themes/cyan.css";
-
 // Components
 export * from "./components/layout/Shell";
 export * from "./components/Button";
--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,50 @@
-import { defineConfig } from 'vite'
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import react from "@vitejs/plugin-react";
+import { glob } from "glob";
+import { fileURLToPath } from "node:url";
+import { extname, relative, resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const copyConfig = viteStaticCopy({
+  targets: [
+    {
+      src: "src/themes",
+      dest: "./",
+    },
+  ],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({ include: ['lib'] })
-  ],
+  plugins: [react(), libInjectCss(), dts({ include: ["lib"] }), copyConfig],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
     },
+
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime', 'yup', 'react-hook-form'],
+      external: ["react", "react/jsx-runtime"],
       input: Object.fromEntries(
         // https://rollupjs.org/configuration-options/#input
-        glob.sync('lib/**/*.{ts,tsx}', { ignore: 'lib/**/*.stories.tsx'}).map(file => [
-          // 1. The name of the entry point
-          // lib/nested/foo.js becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
-          // 2. The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
+        glob
+          .sync("lib/**/*.{ts,tsx}", { ignore: "lib/**/*.stories.tsx" })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
       ),
       output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      }
-    }
-  }
-})
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
